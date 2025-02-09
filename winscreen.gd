e2a90b08de8c679e4caddb82318c2b28c835e1b1
Luke Cochrane <cochranel@outlook.com>
2025-02-09 17:48:49 +0000

    Done

---
+++ winscreen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+# The integer you want to display
+
+
+# Reference to the Label node
+@onready var label = $Label3
+
+func _ready():
+	# Set the initial value of the label
+	update_label()
+
+func update_label():
+	# Update the label text to display the integer
+	label.text = str(PlayerStats.score)
+
+# Example: Increment the score and update the label
+func _process(delta: float) -> void:
+	update_label()
